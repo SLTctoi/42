3d89173726cbcf9051da5e21b808a3fc7dfea167
Maxime Chrispeels <mchrispe@mi-r4-p1.s19.be>
2025-07-07 10:27:36 +0200

    ok

--- pipex/include/pipex.h
+++ pipex/include/pipex.h
@@ -1,22 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mchrispe <mchrispe@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/07 10:27:05 by mchrispe          #+#    #+#             */
+/*   Updated: 2025/07/07 10:27:08 by mchrispe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef PIPEX_H
 # define PIPEX_H
 
 # include <fcntl.h>
-# include <unistd.h>
-# include <stdlib.h>
 # include <stdio.h>
+# include <stdlib.h>
 # include <sys/wait.h>
+# include <unistd.h>
 
 // src/main.c
-void    child_proc(char **av, char **env, int *fd);
-void    parent_proc(char **av, char **env, int *fd);
+void	child_proc(char **av, char **env, int *fd);
+void	parent_proc(char **av, char **env, int *fd);
 
 // src/utils.c
-char    *get_path(char *s, char **env);
-char **get_all_paths(char **env);
-void free_split(char **tab);
-void execute(char *av, char **env);
-void error(void);
+char	*get_path(char *s, char **env);
+char	**get_all_paths(char **env);
+void	free_split(char **tab);
+void	execute(char *av, char **env);
+void	error(void);
 
 // libft
 void	ft_putstr_fd(char *s, int fd);
@@ -25,6 +37,6 @@ char	*ft_strchr(const char *s, int c);
 char	*ft_strjoin(char const *s1, char const *s2);
 size_t	ft_strlen(const char *c);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
-int	ft_strncmp(const char *s1, const char *s2, size_t n);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
 
 #endif
--- pipex/src/utils.c
+++ pipex/src/utils.c
@@ -1,81 +1,92 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mchrispe <mchrispe@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/07 10:26:56 by mchrispe          #+#    #+#             */
+/*   Updated: 2025/07/07 10:26:58 by mchrispe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../include/pipex.h"
 
-// charcher la ligne ou y'a PATH (s) -> on return tous ce qu'il y a sur la ligne apres le path
-char    *get_path(char *s, char **env)
+char	*get_path(char *s, char **env)
 {
-    int i;
-    char *path;
-    char **all_path;
-    char *s_path;
+	int		i;
+	char	*path;
+	char	**all_path;
+	char	*s_path;
 
-    i = 0;
-    all_path = get_all_paths(env);
-    if (!all_path && !s) // ?
-        return (NULL);
-    while (all_path[i])
-    {
-        path = ft_strjoin(all_path[i], "/");
-        s_path = ft_strjoin(path, s);
-        free(path);
-        if (access(s_path, F_OK) == 0)
-        {
-            free_split(all_path);
-            return (s_path);
-        }
-        free(s_path);
-        i++;
-    }
-    free_split(all_path);
-    return (NULL);
+	i = 0;
+	all_path = get_all_paths(env);
+	if (!all_path && !s)
+		return (NULL);
+	while (all_path[i])
+	{
+		path = ft_strjoin(all_path[i], "/");
+		s_path = ft_strjoin(path, s);
+		free(path);
+		if (access(s_path, F_OK) == 0)
+		{
+			free_split(all_path);
+			return (s_path);
+		}
+		free(s_path);
+		i++;
+	}
+	free_split(all_path);
+	return (NULL);
 }
 
-char **get_all_paths(char **env)
+char	**get_all_paths(char **env)
 {
-    int i;
-    char *path;
+	int		i;
+	char	*path;
 
-    i = 0;
-    while (env[i] && ft_strnstr(env[i], "PATH",4) == NULL)
-        i++;
-    if (!env[i])
-        return (NULL);
-    path = ft_strchr(env[i], '=');
-    if (path != NULL)
-        path++;
-    return (ft_split(path, ':'));
+	i = 0;
+	while (env[i] && ft_strnstr(env[i], "PATH", 4) == NULL)
+		i++;
+	if (!env[i])
+		return (NULL);
+	path = ft_strchr(env[i], '=');
+	if (path != NULL)
+		path++;
+	return (ft_split(path, ':'));
 }
 
-void free_split(char **tab)
+void	free_split(char **tab)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while (tab[i])
-    {
-        free(tab[i]);
-        i++;
-    }
-    free(tab);
+	i = 0;
+	while (tab[i])
+	{
+		free(tab[i]);
+		i++;
+	}
+	free(tab);
 }
 
-void execute(char *av, char **env)
+void	execute(char *av, char **env)
 {
-    char *path;
-    char **cmd;
+	char	*path;
+	char	**cmd;
 
-    cmd = ft_split(av, ' ');
-    path = get_path(cmd[0], env);
-    if (!path)
-    {
-        free_split(cmd);
-        error();
-    }
-    if (execve(path, cmd, env) == -1)
-        error();
+	cmd = ft_split(av, ' ');
+	path = get_path(cmd[0], env);
+	if (!path)
+	{
+		free_split(cmd);
+		error();
+	}
+	if (execve(path, cmd, env) == -1)
+		error();
 }
 
-void error(void)
+void	error(void)
 {
-    perror("Error");
-    exit(1);
+	perror("Error");
+	exit(1);
 }
