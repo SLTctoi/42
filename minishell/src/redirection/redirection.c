00b7403daf747d7d2703f29d1dd9a2015e1b7068
Maxime Chrispeels <mchrispe@mi-c4-p5.s19.be>
2025-08-26 15:20:07 +0200

    ok

--- minishell/src/redirection/redirection.c
+++ minishell/src/redirection/redirection.c
@@ -24,36 +24,110 @@
 // 0 if ok and -1 if error
 // use stdin_fileno
 // trouver les bons codes erreur
-int	redirection_stdin(char **input)
+int redirection_loop(char **input)
 {
-	int	i;
-	int	fd;
+    int i;
+    int res;
 
-	i = 1;
-	while (input[i])
-	{
-		if (input[i][0] == '<' && ft_strlen(input[i]) == 1)
-		{
-			fd = open(input[i + 1], O_RDONLY);
-			if (fd < 0)
-			{
-				perror("open");
-				return (-1);
-			}
-			if (dup2(fd, STDIN_FILENO) < 0)
-			{
-				perror("dup2");
-				close(fd);
-				return (-1);
-			}
-			close(fd);
-		}
+    i = 1;
+    while (input[i] && input[i + 1])
+    {
+		if (redirection_stdin(input[i], input[i + 1]) == -1)
+			return (-1);
+		if (double_redirection_stdin(input[i], input[i + 1]) == -1)
+			return (-1);
+		if (redirection_stdout(input[i], input[i + 1]) == -1)
+			return (-1);
 		i++;
+    }
+    return 0;
+}
+
+int	redirection_stdin(char *input ,char *file)
+{
+	int	fd;
+	
+	if (ft_strcmp(input, "<"))
+	{
+		fd = open(file, O_RDONLY);
+		if (fd < 0)
+		{
+			perror("open");
+			return (-1);
+		}
+		if (dup2(fd, STDIN_FILENO) < 0)
+		{
+			perror("dup2");
+			close(fd);
+			return (-1);
+		}
+		close(fd);
 	}
 	return (0);
 }
-
-int double_redirection_stdin(const char *delim)
+// on readline -> on stock dans un fichier temp -> on stdin dans le fichier temp -> on unlink le fichier temp
+int double_redirection_stdin(char *input, const char *delim)
 {
-    
+	int pipefd[2];
+	char *line;
+
+    if (ft_strcmp(input, "<<") == 0)
+	{
+		if (pipe(pipefd) == -1)
+		{
+			perror("pipe");
+			return (-1);
+		}
+		while (1)
+		{
+			line = readline("heredoc> ");
+			if (!line)
+				break;
+			if (ft_strcmp(line, delim) == 0)
+			{
+				free(line);
+				break;
+			}
+			write(pipefd[1], line, ft_strlen(line));
+			write(pipefd[1], "\n", 1);
+			free(line);
+		}
+		close(pipefd[1]);
+		if (dup2(pipefd[0], STDERR_FILENO < 0))
+		{
+			perror("dup2");
+			close(pipefd[0]);
+			return (-1);
+		}
+	}
+	close(pipefd[0]);
+	return (0);
+}
+
+int redirection_stdout(char *input, char *file)
+{
+	int fd;
+
+	if (input[0] == '>')
+	{
+		if (ft_strcmp(input, ">") == 0)
+			fd = open(file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+		else if (ft_strcmp(input, ">>") == 0)
+			fd = open(file, O_WRONLY | O_CREAT | O_APPEND, 0644);
+		else
+			return (-1);
+		if (fd < 0)
+		{
+			perror("open");
+			return (-1);
+		}
+		if (dup2(fd, STDOUT_FILENO) < 0)
+		{
+			perror("dup2");
+			close(fd);
+			return (-1);
+		}
+		close(fd);
+	}
+	return (0);
 }
